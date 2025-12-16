b48b6763680d1864e4e4d85045035acde76e4ebb
Hashim <hashim200222@gmail.com>
2025-12-15 21:34:44 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { PostHogProvider } from "@/contexts/PostHogContext";
@@ -327,6 +328,7 @@ const App = () => (
       </TooltipProvider>
     </QueryClientProvider>
     </ThemeProvider>
+    <Analytics />
   </ErrorBoundary>
 );
 
